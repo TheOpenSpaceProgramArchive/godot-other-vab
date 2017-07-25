87db5373263423f7ef02570310bb0dd4ec982c87
ArchWizard56 <stephen56swnsn@gmail.com>
2017-07-25 16:28:53 -0700

    Added OSC

---
+++ src/scripts/global.gd
@@ -0,0 +1,15 @@
+extends Node
+var currentScene = null
+var usedRocket = null
+func _ready():
+	currentScene = get_tree().get_root().get_child(get_tree().get_root().get_child_count() -1)
+	pass
+func setScene(scene):
+   #clean up the current scene
+   currentScene.queue_free()
+   #load the file passed in as the param "scene"
+   var s = ResourceLoader.load(scene)
+   #create an instance of our scene
+   currentScene = s.instance()
+   # add scene to root
+   get_tree().get_root().add_child(currentScene)
