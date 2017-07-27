b6c0352080febc3ee42e527cc5c1f0cf185bc18c
ArchWizard56 <stephen56swnsn@gmail.com>
2017-07-27 13:52:18 -0700

    Now creating a button for each part

--- src/scripts/VAB.gd
+++ src/scripts/VAB.gd
@@ -13,9 +13,6 @@ class part:
 		description = des
 		manufacturer = man
 		path = pa
-		var button = Button.new()
-		button.set_text(name)
-		get_node("gui/partSelect/"+type+"/"+type+"/"+type).add_child(button)
 func list_files_in_directory(path):
 	var files = []
 	var dir = Directory.new()
@@ -40,6 +37,10 @@ func loadParts():
 		pi.queue_free()
 func _ready():
 	loadParts()
+	for i in classList:
+		var button = Button.new()
+		button.set_text(i.name)
+		get_node("gui/partSelect/"+i.type+"/"+i.type+"/"+i.type).add_child(button)
 	set_fixed_process(true)
 func _fixed_process(delta):
 	if get_node("gui/toolbar/exitButton").is_pressed():
