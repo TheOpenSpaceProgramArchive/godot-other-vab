3e4824494456bea792d1038e172617096062460d
ArchWizard56 <stephen56swnsn@gmail.com>
2017-07-28 21:31:38 -0700

    Mouse drag now moves camera

--- src/scripts/mouseCam.gd
+++ src/scripts/mouseCam.gd
@@ -1,8 +1,7 @@
-extends Camera
+#extends Spatial
 
 var pressed = false
 var last_position = Vector2()
-
 
 func _ready():
     set_process_unhandled_input(true)
@@ -16,6 +15,6 @@ func _unhandled_input(event):
     elif event.type == InputEvent.MOUSE_MOTION and pressed:
         var delta = event.pos - last_position
         last_position = event.pos
-        get_node(".").rotate_y(-delta.x * 0.01)
-        get_node("innergimbal").rotate_y(-delta.x * 0.01)
+        self.rotate_y(delta.x * 0.01)
+        get_node("innergimbal").rotate_x(delta.y * 0.01)
 		
